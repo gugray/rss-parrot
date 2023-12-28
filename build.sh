d4b21026dd5dbdf5a9a80fda93c462a05477548d
gugray <gabor@berlin.de>
2023-12-28 19:18:19 +0100

    ci build fix

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 rm -rf bin
 mkdir bin
 mkdir bin/www
+cp -r src/server/www bin/www
 export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=1
 go build -C src/server -o ../../bin/rss_parrot
-cp -r src/server/www bin/www
