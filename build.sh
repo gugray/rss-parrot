6f1371ceb7d40592c85b8752f2c927b7e417fb28
gugray <gabor@berlin.de>
2023-12-28 18:23:33 +0100

    fix build script after src move to subfolder

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ rm -rf bin
 export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=0 # Needed to run on Alpine
-go build -C src -o ../bin/rss_parrot
-cp -r src/www bin/www
+go build -C src/server -o ../../bin/rss_parrot
+cp -r src/server/www bin/www
