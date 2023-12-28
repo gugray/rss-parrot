55709479037ddccbbca1958ae9c4abdde76f25e3
gugray <gabor@berlin.de>
2023-12-28 18:30:19 +0100

    fix build with CGO

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:latest
 
+RUN apk add --no-cache git make build-base
+
 WORKDIR /app
 COPY bin /app/
 RUN chmod +x ./rss_parrot
--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 rm -rf bin
+mkdir bin
+mkdir bin/www
 export GOOS=linux
 export GOARCH=amd64
-export CGO_ENABLED=0 # Needed to run on Alpine
+export CGO_ENABLED=1 # Needed to run on Alpine
 go build -C src/server -o ../../bin/rss_parrot
 cp -r src/server/www bin/www
