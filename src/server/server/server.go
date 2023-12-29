1b5a0d0fe67b3b34d097be385a474c992b4b6565
gugray <gabor@berlin.de>
2023-12-29 18:43:48 +0100

    cache control

--- src/server/server/server.go
+++ src/server/server/server.go
@@ -11,6 +11,10 @@ import (
 	"strings"
 )
 
+const assetsDir = "/assets/"
+const strCacheControlHdr = "Cache-Control"
+const strCacheControlNoChacheVal = "max-age=31536000, immutable"
+
 var staticFS = http.FileServer(http.Dir("./www/"))
 
 func NewHTTPServer(cfg *shared.Config, logger shared.ILogger, lc fx.Lifecycle, router *mux.Router) *http.Server {
@@ -39,6 +43,7 @@ func NewMux(groups []IHandlerGroup, logger shared.ILogger) *mux.Router {
 	for _, group := range groups {
 		subRouter := router.PathPrefix(group.Prefix()).Subrouter()
 		authMW := group.AuthMW()
+		subRouter.Use(noCacheMW)
 		subRouter.Use(authMW)
 		for _, def := range group.GroupDefs() {
 			subRouter.HandleFunc(def.pattern, def.handler).Methods("OPTIONS", def.method)
@@ -51,6 +56,13 @@ func NewMux(groups []IHandlerGroup, logger shared.ILogger) *mux.Router {
 	return router
 }
 
+func noCacheMW(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set(strCacheControlHdr, strCacheControlNoChacheVal)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func handleStatic(logger shared.ILogger, w http.ResponseWriter, r *http.Request) {
 
 	logNonOK := func(code int) {
@@ -61,25 +73,34 @@ func handleStatic(logger shared.ILogger, w http.ResponseWriter, r *http.Request)
 		logger.Infof("%s request had status %d: %s%s", r.Method, code, r.URL.Path, query)
 	}
 
+	isAsset := strings.HasPrefix(r.URL.Path, assetsDir)
 	if r.Method == "GET" && r.URL.Path != "/" && strings.HasSuffix(r.URL.Path, "/") {
 		logNonOK(403)
 		http.Error(w, dirListNotAllowed, 403)
 		return
 	}
 
-	cw := capturingResponseWriter{w, http.StatusOK}
+	cw := staticFileResponseWriter{w, http.StatusOK, isAsset}
 	staticFS.ServeHTTP(&cw, r)
 	if cw.statusCode >= 400 {
 		logNonOK(cw.statusCode)
 	}
 }
 
-type capturingResponseWriter struct {
+type staticFileResponseWriter struct {
 	http.ResponseWriter
 	statusCode int
+	cached     bool
 }
 
-func (lrw *capturingResponseWriter) WriteHeader(code int) {
+func (lrw *staticFileResponseWriter) WriteHeader(code int) {
 	lrw.statusCode = code
+	if lrw.cached {
+		lrw.ResponseWriter.Header().Set(strCacheControlHdr, strCacheControlNoChacheVal)
+	} else {
+		lrw.ResponseWriter.Header().Set(strCacheControlHdr, "no-cache, no-store, must-revalidate")
+		lrw.ResponseWriter.Header().Set("Pragma", "no-cache")
+		lrw.ResponseWriter.Header().Set("Expires", "0")
+	}
 	lrw.ResponseWriter.WriteHeader(code)
 }
